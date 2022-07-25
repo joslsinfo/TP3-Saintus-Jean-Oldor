bac83c982a6adeedbbbd50e4709c62f8c5726386
Jean Oldor Saintus <josaintus@gmail.com>
2022-07-25 19:25:02 -0400

    service worker

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,20 @@
 //Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v1';
 //Add list of files to cache here.
-const FILES_TO_CACHE = [
-];
-
-// ========================
-
-self.addEventListener('activate', (evt) => {
-    console.log('[ServiceWorker] Activate');
-    //Remove previous cached data from disk.
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+    // Precache static resources here.
     evt.waitUntil(
-    caches.keys().then((keyList) => {
-    return Promise.all(keyList.map((key) => {
-    if (key !== CACHE_NAME) {
-    console.log('[ServiceWorker] Removing old cache',
-   key);
-    return caches.delete(key);
-    }
-    }));
+    caches.open(CACHE_NAME).then((cache) => {
+    console.log('[ServiceWorker] Pre-caching offline page');
+    return cache.addAll(FILES_TO_CACHE);
     })
     );
-    self.clients.claim();
-   })
-
-
-// =================================
-
-
+    self.skipWaiting();
+   });
+   
+const FILES_TO_CACHE = [
+];
 self.addEventListener('install', (evt) => {
  console.log('[ServiceWorker] Install');
  // Precache static resources here.
@@ -40,4 +28,4 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
  console.log('[ServiceWorker] Fetch', evt.request.url);
  //Add fetch event handler here.
-});
+})
